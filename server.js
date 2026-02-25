34a4279028b87df63e74a2a164acf0827e798bbe
Spider-boy19 <Chrisematt04@gmail.com>
2026-02-25 04:05:03 -0500

    Added Static route and lopped image to index page

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express(); //Calling express as a function sets up server
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+app.get('/',
+(req, res)=>{
+console.log('Here');
+res.render('index', {userName: "George"});
+});
+app.get('/users', (req, res)=>{
+res.send('User List');
+});
+app.get('/users/new', (req, res)=>{
+res.send('User New Form');
+});
+app.listen(3030);
