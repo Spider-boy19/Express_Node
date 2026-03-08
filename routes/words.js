3340f18c0ebae37e11dfaa7138f836af702cea46
Spider-boy19 <Chrisematt04@gmail.com>
2026-03-08 17:07:48 -0400

    Created Word Finding function and showed on page

--- routes/words.js
+++ routes/words.js
@@ -1,12 +1,30 @@
-const express = requirw('express');
+const express = require('express');
 const router = express.Router();
-
+const {readFile, writeFile} = require('fs').promises;
 
 router.get('/', (req, res)=>{
     res.send('Word Home Page');
 });
-router.get('/wotd', (req, res)=>{
-    res.send('Word of the Day Page');
+router.get('/wotd', async (req, res)=>{
+    let wordArray = await getWordFromDictinary();
+    let [word, part, definition] = wordArray;
+     res.render('wotd', {word:word, part:part, definition:definition});
+     //Do something with that function up here
 });
 
 module.exports = router;
+
+let getWordFromDictinary = async ()=>{
+    try{
+        const data = await readFile('resorces/allwords.txt', 'utf8');
+        let lines = data.split('\n');
+        let randomNumber = parseInt(Math.random()*lines.length);
+        let randomLine = lines[randomNumber];
+        let wordArray = randomLine.split('\t');
+        console.log(wordArray);
+        return wordArray;
+    }
+    catch(err){
+        console.log("there was an error reading the file", err);
+    }
+};
--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express(); //Calling express as a function sets up server
-const userRouter = require('.routes/users');
+const userRouter = require('./routes/users');
+
 const wordRouter = require('./routes/words');
+
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 app.use('/users', userRouter);
-app.user('/words', wordRouter);
+
+app.use('/words', wordRouter);
+
 app.get('/',
 (req, res)=>{
     console.log('Here');
